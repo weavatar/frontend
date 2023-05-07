9c6550ab56a096e34f3bcbc636641669258923fe
耗子 <i@haozi.net>
2023-05-07 13:15:10 +0800

    feat: 百度统计

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { MotionPlugin } from '@vueuse/motion'
+import baiduAnalytics from 'vue-baidu-analytics'
 
 import App from './App.vue'
 import router from './router'
@@ -12,5 +13,10 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(MotionPlugin)
+app.use(baiduAnalytics, {
+  router: router,
+  siteIdList: ['16b7dd95f598c4ddc6677b1dc4a7a490'],
+  isDebug: false
+})
 
 app.mount('#app')
